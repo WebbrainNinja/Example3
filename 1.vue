0b15a9c226804c4d12b78c9e7a1046a5760fb682
Surendra Kumar Yadav <157869032+WebbrainNinja@users.noreply.github.com>
2024-03-12 11:03:25 +0530

    1.vue

--- 1.vue
+++ 1.vue
@@ -1,1 +1,13 @@
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="pic.jpg" width="125" height="125" />
 
+    <div class="wrapper">
+      <HelloWorld msg="Hello World!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
